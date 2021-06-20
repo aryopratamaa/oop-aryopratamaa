b894bde0d48b2aa7f40a356293ea3d00797d07bb
aryopratamaa <aryo.guest4210@gmail.com>
2021-06-20 15:06:53 +0700

    first

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'aryopratama/oop-aryopratamaa',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'aryopratama/oop-aryopratamaa' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
