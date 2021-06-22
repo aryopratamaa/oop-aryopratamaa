6ad5c6d6909ea335b12bb4051c0244788d0bb800
aryopratamaa <aryo.guest4210@gmail.com>
2021-06-22 15:55:59 +0700

    commit

--- layout/dashboard.php
+++ layout/dashboard.php
@@ -1,57 +1,163 @@
 <!DOCTYPE html>
-<html lang="en"> 
+<html lang="en">
+
 <head>
-    <title>YoDesain</title>
-    
-    <!-- Meta -->
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <meta name="description" content="Portal - Bootstrap 5 Admin Dashboard Template For Developers">
-    <meta name="author" content="Xiaoying Riley at 3rd Wave Media">    
-    <link rel="shortcut icon" href="favicon.ico"> 
-    
-    <!-- FontAwesome JS-->
-    <script defer src="layout/assets/plugins/fontawesome/js/all.min.js"></script>
-    
-    <!-- App CSS -->  
-    <link id="theme-style" rel="stylesheet" href="layout/assets/css/portal.css">
+	<title>YoDesain</title>
 
-</head> 
+	<!-- Meta -->
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<body class="app">   	
-    <header class="app-header fixed-top">	   	            
-        <?php include "header.php"; ?>
-        <?php include "sitebar.php"; ?>
-    </header><!--//app-header-->
-    
-    <div class="app-wrapper">
-	    
-	    <div class="app-content pt-3 p-md-3 p-lg-4">
-		    <div class="container-xl">
-			    <?php include "content_dashboard.php"; ?>
-		    </div><!--//container-fluid-->
-	    </div><!--//app-content-->
-	    
-	    <footer class="app-footer">
-		    <?php include "footer.php"; ?>
-	    </footer><!--//app-footer-->
-	    
-    </div><!--//app-wrapper-->    					
+	<meta name="description" content="Portal - Bootstrap 5 Admin Dashboard Template For Developers">
+	<meta name="author" content="Xiaoying Riley at 3rd Wave Media">
+	<link rel="shortcut icon" href="favicon.ico">
 
- 
-    <!-- Javascript -->          
-    <script src="layout/assets/plugins/popper.min.js"></script>
-    <script src="layout/assets/plugins/bootstrap/js/bootstrap.min.js"></script>  
+	<!-- FontAwesome JS-->
+	<script defer src="layout/assets/plugins/fontawesome/js/all.min.js"></script>
 
-    <!-- Charts JS -->
-    <script src="layout/assets/plugins/chart.js/chart.min.js"></script> 
-    <script src="layout/assets/js/index-charts.js"></script> 
-    
-    <!-- Page Specific JS -->
-    <script src="layout/assets/js/app.js"></script> 
+	<!-- App CSS -->
+	<link id="theme-style" rel="stylesheet" href="layout/assets/css/portal.css">
+
+</head>
+
+<body class="app">
+	<header class="app-header fixed-top">
+		<?php include "header.php"; ?>
+		<div id="app-sidepanel" class="app-sidepanel">
+			<div id="sidepanel-drop" class="sidepanel-drop"></div>
+			<div class="sidepanel-inner d-flex flex-column">
+				<a href="#" id="sidepanel-close" class="sidepanel-close d-xl-none">&times;</a>
+				<div class="app-branding">
+					<a class="app-logo" href="index.php"><img class="logo-icon me-2" src="layout/assets/images/logo.png"
+							alt="logo"><span class="logo-text">YO-DESAIN</span></a>
+				</div>
+				<!--//app-branding-->
+
+				<nav id="app-nav-main" class="app-nav app-nav-main flex-grow-1">
+					<ul class="app-menu list-unstyled accordion" id="menu-accordion">
+						<li class="nav-item has-submenu">
+							<!--//Bootstrap Icons: https://icons.getbootstrap.com/ -->
+							<a class="nav-link active submenu-toggle" href="#" data-bs-toggle="collapse"
+								data-bs-target="#submenu-2" aria-expanded="false" aria-controls="submenu-2">
+								<span class="nav-icon">
+									<!--//Bootstrap Icons: https://icons.getbootstrap.com/ -->
+									<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-house-door"
+										fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+										<path fill-rule="evenodd"
+											d="M7.646 1.146a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 .146.354v7a.5.5 0 0 1-.5.5H9.5a.5.5 0 0 1-.5-.5v-4H7v4a.5.5 0 0 1-.5.5H2a.5.5 0 0 1-.5-.5v-7a.5.5 0 0 1 .146-.354l6-6zM2.5 7.707V14H6v-4a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 .5.5v4h3.5V7.707L8 2.207l-5.5 5.5z" />
+										<path fill-rule="evenodd"
+											d="M13 2.5V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z" />
+									</svg>
+								</span>
+								<span class="nav-link-text">All Menu</span>
+								<span class="submenu-arrow">
+									<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-chevron-down"
+										fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+										<path fill-rule="evenodd"
+											d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z" />
+									</svg>
+								</span>
+								<!--//submenu-arrow-->
+							</a>
+							<!--//nav-link-->
+							<div id="submenu-2" class="collapse submenu submenu-2" data-bs-parent="#menu-accordion">
+								<ul class="submenu-list list-unstyled">
+									<li class="submenu-item"><a class="nav-link"
+											href="index.php?page=kategori_tampil">Categories</a></li>
+									<li class="submenu-item"><a class="nav-link" href="signup.html">Posts</a></li>
+									<li class="submenu-item"><a class="nav-link" href="reset-password.html">Photos</a>
+									</li>
+									<li class="submenu-item"><a class="nav-link" href="404.html">Album</a></li>
+								</ul>
+							</div>
+						</li>
+						<!--//nav-item-->
+						<li class="nav-item">
+							<!--//Bootstrap Icons: https://icons.getbootstrap.com/ -->
+							<a class="nav-link" href="#">
+								<span class="nav-icon">
+									<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-question-circle"
+										fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+										<path fill-rule="evenodd"
+											d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+										<path
+											d="M5.255 5.786a.237.237 0 0 0 .241.247h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286zm1.557 5.763c0 .533.425.927 1.01.927.609 0 1.028-.394 1.028-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94z" />
+									</svg>
+								</span>
+								<span class="nav-link-text">Help</span>
+							</a>
+							<!--//nav-link-->
+						</li>
+						<!--//nav-item-->
+					</ul>
+					<!--//app-menu-->
+				</nav>
+				<!--//app-nav-->
+				<div class="app-sidepanel-footer">
+					<nav class="app-nav app-nav-footer">
+						<ul class="app-menu footer-menu list-unstyled">
+							<li class="nav-item">
+								<!--//Bootstrap Icons: https://icons.getbootstrap.com/ -->
+								<a class="nav-link">
+									<span class="nav-icon">
+										<div class="small">Welcome to:</div>
+										Website <b>Yo-Desain</b>
+									</span><br><br>
+								</a>
+								<!--//nav-link-->
+							</li>
+							<!--//nav-item-->
+						</ul>
+						<!--//footer-menu-->
+					</nav>
+				</div>
+				<!--//app-sidepanel-footer-->
+
+			</div>
+			<!--//sidepanel-inner-->
+		</div>
+		<!--//app-sidepanel-->
+	</header>
+	<!--//app-header-->
+
+	<div class="app-wrapper">
+
+		<div class="app-content pt-3 p-md-3 p-lg-4">
+			<div class="container-xl">
+				<?php
+					if (isset($_GET['page'])){
+						include $_GET['page'] . ".php";
+					} else {
+						include "content_dashboard.php";
+					}
+				?>
+
+			</div>
+			<!--//container-fluid-->
+		</div>
+		<!--//app-content-->
+
+		<footer class="app-footer">
+			<?php include "footer.php"; ?>
+		</footer>
+		<!--//app-footer-->
+
+	</div>
+	<!--//app-wrapper-->
+
+
+	<!-- Javascript -->
+	<script src="layout/assets/plugins/popper.min.js"></script>
+	<script src="layout/assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+
+	<!-- Charts JS -->
+	<script src="layout/assets/plugins/chart.js/chart.min.js"></script>
+	<script src="layout/assets/js/index-charts.js"></script>
+
+	<!-- Page Specific JS -->
+	<script src="layout/assets/js/app.js"></script>
 
 </body>
-</html> 
 
+</html>
